b69f00da26757afe4201a13c287c3f16d0fe8b52
sandrine <sandrine.rodriguez@hotmail.com>
2019-10-21 17:41:36 +0200

    Update route admin-edit-user =>  { path: 'admin-edit-user/:url', component: AdminEditUserComponent }

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,8 @@ const routes: Routes = [
   { path: 'adminApplis', component: AdminApplisComponent },
   { path: 'adminContents', component: AdminContentsComponent },
   { path: 'adminUsers', component: AdminUsersComponent },
-  { path: 'admin-edit-user/:id', component: AdminEditUserComponent },
+  // { path: 'admin-edit-user/:id', component: AdminEditUserComponent },
+  { path: 'admin-edit-user/:url', component: AdminEditUserComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'userApps', component: AdminUsersComponent }
 ];

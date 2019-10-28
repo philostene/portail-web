548a9f4f91ce60df0ec1c4657c81bafbcfb9047a
sandrine <sandrine.rodriguez@hotmail.com>
2019-10-28 12:49:14 +0100

    Add method for add and delete Appli of user
    update getUserApplis(user:User)
    getAllApplis()
    updateUserApplis(event: MatOptionSelectionChange, appli: Appli) 
    addInCurrentApplis(appli: Appli) 
    deleteInCurrentApplis(appli: Appli) 
    isInCurrentApplis(appli: Appli)

--- src/app/admin/admin-edit-user/admin-edit-user.component.ts
+++ src/app/admin/admin-edit-user/admin-edit-user.component.ts
@@ -3,10 +3,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { PortailService } from 'src/app/services/portail.service';
 import { User } from 'src/app/models/User';
 import { Role } from 'src/app/models/Role';
-import { Roles } from 'src/app/models/Roles';
-import { Subscription } from 'rxjs';
-import { ApplisComponent } from 'src/app/applis/applis.component';
-import { Applis } from 'src/app/models/Applis';
 import { Appli } from 'src/app/models/Appli';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { MatOptionSelectionChange } from '@angular/material/core';
@@ -24,6 +20,8 @@ export class AdminEditUserComponent implements OnInit {
   currentApplis: Appli [];
   allRoles: Array<Role>;
   currentRolesIds: number [];
+  currentApplisIds: number [];
+  allApplis: Array<Appli>;
 
   constructor(
     private portailService: PortailService,
@@ -47,6 +45,7 @@ export class AdminEditUserComponent implements OnInit {
           console.error(err);
         });
     this.getAllRoles();
+    this.getAllApplis();
     }
 
     getUserRoles(user: User) {
@@ -125,29 +124,83 @@ export class AdminEditUserComponent implements OnInit {
       return false;
     }
 
-
     getUserApplis(user: User) {
       this.portailService.getApplisByUser(user)
         .subscribe(data => {
             this.currentApplis = data._embedded.applis;
-            console.log('currentApplis : ' );
-            console.log(this.currentApplis);
-
+            this.currentApplisIds = [this.currentApplis.length];
+            this.currentApplis.forEach(appli => {
+              console.log('appli ', appli);
+              this.currentApplisIds.push(appli.id);
+            });
+            // console.log('currentApplis : ' );
+            // console.log(this.currentApplis);
           },
           err => {
           console.error(err);
         });
     }
 
-    /* onUpdateUser(data) {
-      this.authService.updateRegister(data)
-          .subscribe(resp => {
-            // this.mode = 'list';
-            // this.getAllUsers();
-          }, err => {
-            console.error(err);
-          });
-      } */
+    getAllApplis() {
+      this.portailService.getAllApplis()
+        .subscribe(data => {
+            this.allApplis = data._embedded.applis;
+            console.log('allApplis : ' );
+            console.log(this.allApplis);
+          },
+          err => {
+          console.error(err);
+        });
+    }
+
+    // modifie l'appli selectionnée dans la mat-select puis add ou delete appli
+    updateUserApplis(event: MatOptionSelectionChange, appli: Appli) {
+      // appli selectionnée dans les options mat-select
+      let selected = event.source.selected;
+      if (selected === true) {
+        this.addInCurrentApplis(appli);
+      } else {
+        this.deleteInCurrentApplis(appli);
+      }
+      console.log('currentApplis ', this.currentApplis);
+    }
+
+    // si appli selectionnée n'est pas dans la liste des applis du USER, on l'ajoute
+    addInCurrentApplis(appli: Appli) {
+      if (!this.isInCurrentApplis(appli)) {
+      this.currentApplis.push(appli);
+      this.currentApplisIds.push(appli.id);
+      }
+    }
+
+    // si appli est dans la liste des applis du USER, on la décoche et on la supprime
+    deleteInCurrentApplis(appli: Appli) {
+      if (this.isInCurrentApplis(appli)) {
+        // on vérifie que appli est dans la liste des applis de USER (currentApplis)
+        for ( let i = 0; i < this.currentApplis.length; i++) {
+          const idCurrentAppli = this.currentApplis[i].id ;
+          if (idCurrentAppli === appli.id) {
+            // suppression de la liste des applis du user
+            this.currentApplis.splice(i, 1);
+            // suppression de la liste des currentApplisIds du user
+            const index = this.currentApplisIds.indexOf(idCurrentAppli);
+            if (index > -1) {
+              this.currentApplisIds.splice(index, 1);
+            }
+          }
+       }
+      }
+    }
+
+    // Vérifie si appli cochée est dans la liste des applis du user
+    isInCurrentApplis(appli: Appli) {
+      for (let i = 0; i < this.currentApplis.length; i++) {
+        if ( this.currentApplis[i].id === appli.id) {
+         return true;
+        }
+     }
+      return false;
+    }
 
     // update ressources sauf roles et applis
     onUpdateUser(formData) {
@@ -161,24 +214,29 @@ export class AdminEditUserComponent implements OnInit {
           }, err => {
             console.error(err);
           });
-
-
     }
 
     onUpdateUserRoles() {
       let url =  this.portailService.getBaseUrl() + `/updateUserRoles/` + this.currentUser.id ;
 
       this.portailService.putRessource(url, this.currentRoles)
-      .subscribe(resp => {
-       let retour: HttpResponse<any> =resp;
-       console.log(retour);
+          .subscribe(resp => {
+          let retour: HttpResponse<any> = resp;
+          console.log(retour);
 
-      }, err => {
-        console.error(err);
-      });
+          }, err => {
+            console.error(err);
+          });
     }
 
     onUpdateUserApplis() {
-
+      let url = this.portailService.getBaseUrl() + '/updateUserApplis/' + this.currentUser.id;
+      this.portailService.putRessource(url, this.currentApplis)
+          .subscribe(resp => {
+            let retour: HttpResponse<any> = resp;
+            console.log(retour);
+          }, err => {
+            console.error(err);
+          });
     }
 }

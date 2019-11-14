95a7eccc4c7ed8204851b39eee63814112818ff6
sandrine <sandrine.rodriguez@hotmail.com>
2019-11-14 08:52:45 +0100

    add Email model

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from '../models/Email';
+import { EmailService } from '../services/email.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  imgConstruction = 'assets/images/page-en-construction.png';
-  constructor() { }
+  // imgConstruction = 'assets/images/page-en-construction.png';
+  emailUser: Email = new Email();
+
+  constructor(private httpClient: HttpClient, private emailService: EmailService) { }
 
   ngOnInit() {
+  }
+
+  envoyerEmail() {
+    this.emailService.envoyerEmail(this.emailUser)
+        .subscribe(data => {
+          console.log(data);
+          //location.reload();
+        }, err => {
+          console.error(err);
+          alert('Une erreur est survenue lors de l\'envoi du courrier électronique');
+        });
+  }
+
+  onSubmit() {
+    this.envoyerEmail();
   }
 
 }
